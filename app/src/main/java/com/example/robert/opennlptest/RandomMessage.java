09a657045bd7a3e573a107af04fe249bc1a6e427
boacompiler <thegeekiestthingonearth@live.co.uk>
2017-06-15 10:31:55 +0100

    adds javadoc to RandomMessage.java

--- app/src/main/java/com/example/robert/opennlptest/RandomMessage.java
+++ app/src/main/java/com/example/robert/opennlptest/RandomMessage.java
@@ -4,15 +4,18 @@ import java.util.Random;
 
 /**
  * Created by robert on 18/05/17.
+ * a class that allows easy randomisation of voice lines
  */
 
 public class RandomMessage {
+    //list of responses for a uncertain query answer
     private String[] notsure = new String[]{
             "I'm not sure, but this could be it: ",
             "I could be mistaken, but here it is: ",
             "This is probably right: ",
             "I think this is correct: "};
 
+    //list of jokes
     private String[] joke = new String[]{
             "Two hats are sitting on a hat rack. One says, you stay here, I'll go on a head.",
             "Parallel lines have so much in common but it’s a shame they’ll never meet.",
@@ -25,16 +28,29 @@ public class RandomMessage {
 
     };
 
+    /**
+     * Getter for uncertain response
+     * @return random uncertain response
+     */
     public String GetNotSure()
     {
         return RandIndex(notsure);
     }
 
+    /**
+     * Getter for jokes
+     * @return random joke
+     */
     public String GetJoke()
     {
         return RandIndex(joke);
     }
 
+    /**
+     * gets a random entry in an array of strings
+     * @param array string array
+     * @return contents of random index
+     */
     private String RandIndex(String[] array)
     {
         int rnd = new Random().nextInt(array.length);
